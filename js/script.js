94331554739f8fe6293c99efb78325f581cadcfb
Taylor Perkins <taylorwp2018@gmail.com>
2026-07-26 00:28:31 -0400

    Version 1.1 - Hero animations and GIS styling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
-console.log("Taylor Perkins Portfolio Version 1.0 Loaded");
+const titles = [
+
+    "Geospatial Developer",
+
+    "GIS Analyst",
+
+    "Spatial Data Automation",
+
+    "Python Developer",
+
+    "Spatial Intelligence"
+
+];
+
+const typingElement = document.getElementById("typing-text");
+
+let titleIndex = 0;
+
+let characterIndex = 0;
+
+let deleting = false;
+
+function type(){
+
+    const currentTitle = titles[titleIndex];
+
+    if(!deleting){
+
+        typingElement.textContent = currentTitle.substring(0,characterIndex++);
+
+        if(characterIndex > currentTitle.length){
+
+            deleting = true;
+
+            setTimeout(type,1600);
+
+            return;
+
+        }
+
+    }
+
+    else{
+
+        typingElement.textContent = currentTitle.substring(0,--characterIndex);
+
+        if(characterIndex===0){
+
+            deleting=false;
+
+            titleIndex=(titleIndex+1)%titles.length;
+
+        }
+
+    }
+
+    setTimeout(type,deleting?40:80);
+
+}
+
+type();
